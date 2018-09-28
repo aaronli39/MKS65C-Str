8e7545bf4485fe97563439fd8411dcd3ba5bbb2e
Jason Lin <jasonlin43212@gmail.com>
2018-09-28 15:15:45 -0400

    added strlen and a git ignore

---
+++ homeJ.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//PUT THE NAME OF YOUR partner in Li.Aaron.txt
+
+int my_strlen(char * str){
+  int length = 0;
+  while (*str++){
+    length++;
+  }
+  return length;
+}
+
+int my_strncpy(char * ){
+  return 0;
+}
+
+int main() {
+  char *testString = "";
+  printf("%d\n",my_strlen(testString));
+  return 0;
+}
