1452ec3849743e7232a84b28dcea8508764348f6
Jason Lin <jasonlin43212@gmail.com>
2018-09-29 13:35:51 -0400

    test strncpy

--- homeJ.c
+++ homeJ.c
@@ -9,20 +9,42 @@ int my_strlen(char * str){
   return length;
 }
 
-char * my_strncpy(char *dest, char *source, int n){
+char * my_strncpy(char *dest, const char *source, int n){
   int i;
-  for (i=0; i<n && *source; i++){
+  for (i=0; i<n && *(source+i); i++){
     *(dest+i) = *(source+i);
+  }
+  for (;i<n;i++){
+    *(dest+i) = 0;
   }
   return dest;
 }
 
+char * my_strncat(char *dest, char *source, int n){
+  int i;
+  int destLength = my_strlen(dest);
+  for (i=0; i<n && *source; i++){
+
+  }
+}
+
 int main() {
-  char *s1 = "superman";
-  char s2[] = "man";
-  char s3[30] = "people";
-  char *s4 = "earth";
-  char s5[] = "manofearth";
-  printf("%d\n",my_strlen(s5));
+  char *s1 = "warped";
+  char s2[50] = "hello";
+  char s3[] = "help";
+
+  char *my_s1 = "warped";
+  char my_s2[50] = "hello";
+  char my_s3[] = "help";
+
+  printf("Testing strlen(s1):\n");
+  printf("[standard]: %d\n",(int)strlen(s1));
+  printf("[mine]: %d\n",my_strlen(my_s1));
+  printf("\n");
+
+  printf("Testing strncpy(s2,s1,4):\n");
+  printf("[standard]: %s\n",strncpy(s2,s1,4));
+  printf("[mine]: %s\n",my_strncpy(my_s2,my_s1,4));
+
   return 0;
 }
